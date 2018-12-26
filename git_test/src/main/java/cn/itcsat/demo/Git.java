6de513a4aaa4c34bac15cbb600af5d3191a8ddd4
xmart <452651108@qq.com>
2018-12-26 21:14:51 +0800

    本地工程添加到git

---
+++ git_test/src/main/java/cn/itcsat/demo/Git.java
@@ -0,0 +1,15 @@
+package cn.itcsat.demo;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+        System.out.println("git");
+    }
+}
